06b953f35716626b013ab82b36c9536fead95196
Joseph Senders <jasenders@gmail.com>
2018-08-21 14:33:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,6 +15,17 @@ def add_item(name, price, quantity=1)
     end
   end
 
+def apply_discount
+  if discount
+      self.total = self.total - self.total*self.discount/100
+      result = "After the discount, the total comes to $#{self.total}."
+    else
+      result = "There is no discount to apply."
+    end
+    return result
+  end 
+  
+
   def items
     return self.purchases
   end
